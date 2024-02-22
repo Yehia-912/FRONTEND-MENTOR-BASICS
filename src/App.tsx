2bee350093a1f97b62b12e045970328d741e674f
Yehia <yyyyahya852@gmail.com>
2024-02-22 15:22:54 +0200

    Intializing new project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import View from "./components/View";
+
+const App = () => {
+  return (
+    <div>
+      <View start="New Project for learn tailwindcss and react" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/View.tsx
@@ -0,0 +1,9 @@
+interface IProps {
+  start: string;
+}
+
+const View = ({ start }: IProps) => {
+  return <div>{start}</div>;
+};
+
+export default View;
